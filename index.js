5df7b0e1093798f83e84427e885addcc5dc716d9
roylines <roy@roylines.co.uk>
2016-04-27 07:55:12 +0100

    first pass of implementing request counters

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+const client = require('prom-client');
+const metric = {
+  http: {
+    requests: {
+      total: new client.Counter('http_requests_total', 'total number of http requests', ['method', 'path'])
+    }
+  }
+}
+
 function middleware(request, response, done) {
+  metric.http.requests.total.labels(request.method, request.path).inc();
   return done();
 };
 
-module.exports = () => {
-  return middleware;
+function metrics(req, res) {
+  return res.send(client.register.metrics());
+}
+
+function instrument(app) {
+  app.use(middleware);
+  app.get('/metrics', metrics);
+}
+
+module.exports = {
+  instrument: instrument
 }
--- test/index.js
+++ test/index.js
@@ -1,7 +1,34 @@
-const middleware = require('../index')();
+const express = require('express');
+const epithemeus = require('../index');
+const request = require('request');
+const should = require('chai').should();
 
 describe('epimetheus', () => {
-  it('should', (done) => {
-    return done();
+  before((done) => {
+    const app = express();
+    epithemeus.instrument(app);
+    app.get('/', (req, res) => {
+      res.send();
+    });
+    this.server = app.listen(3000, done);
+  });
+
+  after((done) => {
+    return this.server.close(done)
+  });
+
+  it('should return 200 for /', (done) => {
+    request('http://localhost:3000/', (e, r, b) => {
+      r.statusCode.should.equal(200); 
+      return done(e);
+    });
+  });
+  
+  it('should return 200 for /metrics', (done) => {
+    request('http://localhost:3000/metrics/', (e, r, b) => {
+      console.log('BODY', b);
+      r.statusCode.should.equal(200); 
+      return done(e);
+    });
   });
 });
