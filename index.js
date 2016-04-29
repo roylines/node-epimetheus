60223b72ba146857f1ecad2201862b74925f03ec
roylines <roy@roylines.co.uk>
2016-04-29 18:00:58 +0100

    added instrumentation of event loop lag and memory usage

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const hapi = require('./lib/hapi');
 const express = require('./lib/express');
 const restify = require('./lib/restify');
+const eventLoop = require('./lib/event-loop');
+const memoryUsage = require('./lib/memory-usage');
 
 function instrument(app) {
+  eventLoop.instrument();
+  memoryUsage.instrument();
+
   if (hapi.instrumentable(app)) {
     hapi.instrument(app);
   } else if (express.instrumentable(app)) {
