b06e37159b447a7711c079b0e43a9d696b73e02a
YvesPasteur <pro@ypasteur.fr>
2017-05-16 17:39:48 +0200

    use route path instead of url in metric labels

--- test/express.js
+++ test/express.js
@@ -4,6 +4,8 @@ const epithemeus = require('../index');
 const assertExpectations = require('./assert-expectations');
 
 function setup(options) {
+  options.routePath = '/resource/:id';
+
   return describe('express ' + options.url, () => {
     before((done) => {
       const app = express();
@@ -11,7 +13,7 @@ function setup(options) {
       app.get('/', (req, res) => {
         res.send();
       });
-      app.get('/resource/:id', (req, res) => {
+      app.get(options.routePath, (req, res) => {
         res.send();
       });
       this.server = app.listen(3000, done);
