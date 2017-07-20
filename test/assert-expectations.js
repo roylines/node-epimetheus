da92d246473a85b265319ecce001d46380f50033
Tristan Colgate <tristan@qubit.com>
2017-07-20 10:20:05 +0100

    De-lint test code

--- test/assert-expectations.js
+++ test/assert-expectations.js
@@ -1,30 +1,32 @@
-const request = require('request');
-const should = require('chai').should();
+/* global it */
 
-module.exports = function() {
+const request = require('request')
+const should = require('chai').should()
+
+module.exports = function () {
   it('should return 200 for /', (done) => {
     request('http://localhost:3000/', (e, r, b) => {
-      r.statusCode.should.equal(200);
-      return done(e);
-    });
-  });
+      r.statusCode.should.equal(200)
+      return done(e)
+    })
+  })
 
   it('should return 200 for /resource/id', (done) => {
     request('http://localhost:3000/resource/101', (e, r, b) => {
-      r.statusCode.should.equal(200);
-      return done(e);
-    });
-  });
+      r.statusCode.should.equal(200)
+      return done(e)
+    })
+  })
 
   it('should return 200 for /metrics', (done) => {
     request('http://localhost:3000/metrics', (e, r, b) => {
-      r.statusCode.should.equal(200);
-      should.exist(r.headers['content-type']);
-      r.headers['content-type'].should.equal('text/plain; charset=utf-8');
-      b.should.have.string('# HELP ');
-      b.should.have.string('"/resource/:id"');
-      b.should.have.string('status="200"');
-      return done(e);
-    });
-  });
+      r.statusCode.should.equal(200)
+      should.exist(r.headers['content-type'])
+      r.headers['content-type'].should.equal('text/plain; charset=utf-8')
+      b.should.have.string('# HELP ')
+      b.should.have.string('"/resource/:id"')
+      b.should.have.string('status="200"')
+      return done(e)
+    })
+  })
 }
--- test/express.js
+++ test/express.js
@@ -1,61 +1,63 @@
-const request = require('request');
-const should = require('chai').should();
-const express = require('express');
-const epithemeus = require('../index');
-const assertExpectations = require('./assert-expectations');
+/* global describe it before after */
+
+const request = require('request')
+const should = require('chai').should()
+const express = require('express')
+const epithemeus = require('../index')
+const assertExpectations = require('./assert-expectations')
 
 describe('express', () => {
   before((done) => {
-    const app = express();
-    epithemeus.instrument(app);
+    const app = express()
+    epithemeus.instrument(app)
     app.get('/', (req, res) => {
-      res.send();
-    });
+      res.send()
+    })
     app.get('/resource/:id', (req, res) => {
-      res.send();
-    });
-    this.server = app.listen(3000, done);
-  });
+      res.send()
+    })
+    this.server = app.listen(3000, done)
+  })
 
   after((done) => {
     return this.server.close(done)
-  });
+  })
 
-  assertExpectations();
-});
+  assertExpectations()
+})
 
 describe('express with client', () => {
   before((done) => {
-    const client = require('@qutics/prom-client');
-    const app = express();
-    epithemeus.instrumentWithClient(app,client);
-    mycounter = new client.Counter('mymetrics_things_counter_total','a metric')
+    const client = require('@qutics/prom-client')
+    const app = express()
+    epithemeus.instrumentWithClient(app, client)
+    let mycounter = new client.Counter('mymetrics_things_counter_total', 'a metric')
     app.get('/', (req, res) => {
       mycounter.inc()
-      res.send();
-    });
+      res.send()
+    })
     app.get('/resource/:id', (req, res) => {
-      res.send();
-    });
-    this.server = app.listen(3000, done);
-  });
+      res.send()
+    })
+    this.server = app.listen(3000, done)
+  })
 
   after((done) => {
     return this.server.close(done)
-  });
+  })
 
-  assertExpectations();
+  assertExpectations()
 
   it('should countain extra metrics', (done) => {
     request('http://localhost:3000/metrics', (e, r, b) => {
-      r.statusCode.should.equal(200);
-      should.exist(r.headers['content-type']);
-      r.headers['content-type'].should.equal('text/plain; charset=utf-8');
-      b.should.have.string('# HELP ');
-      b.should.have.string('"/resource/:id"');
-      b.should.have.string('status="200"');
-      b.should.have.string('mymetrics_things_counter_total');
-      return done(e);
-    });
-  });
-});
+      r.statusCode.should.equal(200)
+      should.exist(r.headers['content-type'])
+      r.headers['content-type'].should.equal('text/plain; charset=utf-8')
+      b.should.have.string('# HELP ')
+      b.should.have.string('"/resource/:id"')
+      b.should.have.string('status="200"')
+      b.should.have.string('mymetrics_things_counter_total')
+      return done(e)
+    })
+  })
+})
